18046bfb5c3988b8491f96e263e0e44bc53b5c70
Inam Ul Rehman <inam@dryermaster.com>
2024-11-12 13:27:34 -0500

    Add error handling components: Error, GlobalError, and NotFound

---
+++ src/app/error.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React from 'react';
+
+function Error() {
+  return (
+    <div>
+      <h1>Error - Something went wrong</h1>
+    </div>
+  );
+}
+
+export default Error;
---
+++ src/app/global-error.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React from 'react';
+
+function GlobalError() {
+  return (
+    <div>
+      <h1>Error - Something went wrong - Global</h1>
+    </div>
+  );
+}
+
+export default GlobalError;

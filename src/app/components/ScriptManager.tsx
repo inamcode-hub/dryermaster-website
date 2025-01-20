d79e21b7aca049c36135c2af1be08118094119fb
Inam Ul Rehman <inam@dryermaster.com>
2025-01-20 09:28:16 -0500

    SCRUM-25: A script manager for Hubspot added for chat testing and working.

--- src/app/components/ScriptManager.tsx
+++ src/app/components/ScriptManager.tsx
@@ -4,7 +4,10 @@ import useScript from '@/lib/useScript';
 
 const ScriptManager = () => {
   console.log('[ScriptManager]: Initializing script manager.');
-  useScript('https://js-na1.hs-scripts.com/48918185.js', 'hs-script-loader'); // Ensure HTTPS
+  useScript(
+    'https://js-na1.hs-scripts.com/48918185.js',
+    'hs-script-loader-chat'
+  ); // Ensure HTTPS
 
   return null; // No UI; just loads the script
 };
